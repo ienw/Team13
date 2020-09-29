16afa907a92a7a0aaa7f6a8486943462b457addb
ienw <ienw@metropolia.fi>
2020-09-29 18:06:45 +0300

    add permission checks and open street map

--- app/src/main/java/com/example/team13/MainActivity.kt
+++ app/src/main/java/com/example/team13/MainActivity.kt
@@ -1,11 +1,92 @@
 package com.example.team13
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // we use mainactivity just to check permissions
+        checkPermissions()
+    }
+
+    // when permissions are ok we launch actual app
+    private fun launchApp() {
+        if (hasAllPermissions()) {
+            startActivity(Intent(this, App::class.java))
+        }
+    }
+
+    private fun hasAllPermissions(): Boolean {
+        return ActivityCompat.checkSelfPermission(this,
+            Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun checkPermissions() {
+        // storage permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) !=
+            PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ),
+                1
+            )
+        }
+
+        // location permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                            android.Manifest.permission.ACCESS_FINE_LOCATION
+                    ),
+                    0
+            )
+
+            Log.d("asd", "NO PERMISSION FOR LOCATION, ASKING FOR ACCESS")
+            status.text = "ASKING FOR LOCATION PERMISSION"
+            return
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) !=
+            PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ),
+                1
+            )
+        }
+        launchApp()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 0 && grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            // Restart locationupdates if permissions have been granted
+            Log.d("asd", "LOCATION PERMISSION GRANTED")
+            launchApp()
+        } else if (requestCode == 1 && grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            Log.d("asd", "STORAGE PERMISSION GRANTED")
+            launchApp()
+        } else {
+            status.text = "PERMISSION NOT GRANTED"
+        }
     }
 }
